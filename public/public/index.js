3e50c034fb35c6bf0397155776c20335efe0aa0a
Ussen_Kimanuka <ukimanuka@gmail.com>
2023-11-02 23:27:20 +0300

    initial project skeleton

---
+++ public/public/index.js
@@ -0,0 +1,10 @@
+// Api key
+
+const apiKey = "ZXZCmQDuIWv3Hbg7tasJpFxxhQbBuSsU";
+
+
+// define the ids of the elements
+
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchInput");
+const locationInfo = document.getElementById("locationInfo");
